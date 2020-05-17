1ed049c6f802fc050cbb5ad0eed29fe11a7b690d
jmp199 <65424990+jmp199@users.noreply.github.com>
2020-05-16 22:23:42 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ var name = "Hi there! My name is Justin Phillips";
 document.querySelector(".name").innerHTML = name;
 var welcome ="Welcome to CIT 140000";
 document.querySelector(".welcome").innerHTML = welcome;
+
+var num = 20;
+num=num+19;
+document.querySelector(".num").innerHTML = num;
